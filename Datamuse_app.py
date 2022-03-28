d1cb78628cb80ddf4906f7e27c57db4a2ac20256
Vstrijova <101127668+Vstrijova@users.noreply.github.com>
2022-03-28 16:50:33 +0200

    Update Datamuse_app.py

--- Datamuse_app.py
+++ Datamuse_app.py
@@ -1,30 +1,29 @@
 import streamlit as st
+import json, requests 
 
 st.markdown("Welcome to **MyDictionary**")
 
-import json, requests 
 
-yourword = st.text_input('Enter a word:', 'sky')
+
+keyword = st.text_input('Enter a word:', 'sky')
 option = st.selectbox('What are you looking for?',('Meaning', 'Synonyms', 'Antonyms'))
 
-#st.write('You selected:', option)
-url1 = 'https://api.datemuse.com/words?&ml=' + yourword + '&max=3'
-url2 = 'https://api.datemuse.com/words?&rel_syn=' + yourword + '&max=3'
-url3 = 'https://api.datemuse.com/words?&rel_ant=' + yourword + '&max=3'
+url_a = 'https://api.datamuse.com/words?ml=' + keyword + '&max=5'
+url_b = 'https://api.datamuse.com/words?rel_syn=' + keyword + '&max=5'
+url_c = 'https://api.datamuse.com/words?rel_ant=' + keyword + '&max=5'
 
-yourword1 = requests.get(url1)
-yourword2 = requests.get(url2)
-yourword3 = requests.get(url3)
-
-yourworddef = json.loads(yourword1.text)
-yourwordsyn = json.loads(yourword2.text)
-yourwordant = json.loads(yourword3.text)
+response_a = requests.get(url_a)
+dataFromDatamuse_a = json.loads(response_a.text) 
+response_b = requests.get(url_b)
+dataFromDatamuse_b = json.loads(response_b.text)
+response_c = requests.get(url_c)
+dataFromDatamuse_c = json.loads(response_c.text)
 
 if option == 'Meaning':
-  st.write(yourworddef)
+  st.write(dataFromDatamuse_a)
 elif option == 'Synonyms':
-  st.write(yourwordsyn)
+  st.write(dataFromDatamuse_b)
 elif option == 'Antonyms':
-  st.write(yourwordant)
+  st.write(dataFromDatamuse_c)
 else:
   pass
